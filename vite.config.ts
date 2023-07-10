b4c20ac73fa814375f7d734c80505decd810d6df
vincent.wen <wenziyangday@gmail.com>
2023-07-10 13:32:33 +0800

    chore:设置环境变量的config配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,36 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import eslintPlugin from 'vite-plugin-eslint'
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
-const { env } = import.meta
-export default defineConfig({
-  plugins: [
-    vue(),
-    eslintPlugin({ include: ['src/**/*.js', 'src/**/*.vue', 'src/**/*.ts', 'src/*.js', 'src/*.vue', 'src/*.ts', 'vite.config.ts'] })
-  ],
-  css: {},
-  server: {
-    host: '0.0.0.0',
-    port: 9600,
-    cors: true,
-    open: true,
-    // 反向代理
-    proxy: {
-      [env.VITE_APP_BASE_API]: {
-        target: env.VITE_APP_BASE_API_URL,
-        changeOrigin: true,
-        pathRewrite: {
-          // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-          [`^${env.VITE_APP_BASE_API}`]: ''
+export default ({ mode }): any => {
+  // 参数mode为开放模式或生产模式
+  const env = loadEnv(mode, process.cwd()); // 获取.env文件里定义的环境变量
+  return defineConfig({
+    plugins: [
+      vue(),
+      eslintPlugin({
+        include: [
+          'src/**/*.js',
+          'src/**/*.vue',
+          'src/**/*.ts',
+          'src/*.js',
+          'src/*.vue',
+          'src/*.ts',
+          'vite.config.ts'
+        ]
+      })
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_APP_API,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
         }
       }
-    }
-  }
-})
+    },
+    // 项目部署在主域名的子文件使用,例如http://localhost:3000/myvite/。不填默认就是/
+    base: env.VITE_APP_BASE_URL ?? '/'
+  });
+};
