57d71bfa54e4cfb854aa22bc2de0e2e6de084d74
vincent.wen <wenziyangday@gmail.com>
2023-08-09 22:49:34 +0800

    feat: 单条描述值

--- src/components/confirm-content/ConfirmContent.vue
+++ src/components/confirm-content/ConfirmContent.vue
@@ -6,17 +6,20 @@ withDefaults(
   defineProps<{
     msg?: string;
     optTypeCN?: OptTypeCN;
+    word?: string;
   }>(),
   {
     msg: '1',
     icon: 'warning-outlined',
-    optTypeCN: '删除'
+    optTypeCN: '删除',
+    word: ''
   }
 );
 </script>
 
 <template>
-  <span>是否确认{{ optTypeCN }}编号为{{ msg }}的数据项？</span>
+  <span v-if="word">{{ word }}</span>
+  <span v-else>是否确认{{ optTypeCN }}编号为{{ msg }}的数据项？</span>
 </template>
 
 <style scoped lang="less">
