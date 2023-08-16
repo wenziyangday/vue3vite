7522082b069d838b8029c8f4963ef8d0c6bee80a
vincent.wen <wenziyangday@gmail.com>
2023-08-16 16:48:37 +0800

    feat: iconfont组件封装

---
+++ src/components/icon-font/VWIconFont.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { createFromIconfontCN } from '@ant-design/icons-vue';
+import { ref } from 'vue';
+
+import constants from '@/constants/constants';
+
+const IconFont = createFromIconfontCN({
+  scriptUrl: constants.iconFontJsUrl
+});
+
+const props = withDefaults(
+  defineProps<{
+    type: string | undefined;
+    fontSize?: number;
+    fontColor?: string;
+  }>(),
+  {
+    fontSize: 12,
+    fontColor: '#000000'
+  }
+);
+
+const style = ref({ fontSize: `${props.fontSize}px`, color: props.fontColor });
+</script>
+<template>
+  <icon-font :type="type" :style="style" />
+</template>
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,12 +5,14 @@ interface Constants {
   token: string;
   tokenPrefix: string;
   repeatSubmit: string;
+  iconFontJsUrl: string;
 }
 
 const constants: Constants = {
   token: 'ADMIN_TOKEN',
   tokenPrefix: 'Bearer',
-  repeatSubmit: 'RS_OBJ'
+  repeatSubmit: 'RS_OBJ',
+  iconFontJsUrl: '//at.alicdn.com/t/c/font_2133797_n4vri45rer.js'
 };
 
 export default constants;
